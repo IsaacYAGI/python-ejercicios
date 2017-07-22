7e4af26849732e601f0e1e1f9b461e2ad35a83f0
Isaac Yriarte <isaacyriarte@gmail.com>
2017-07-22 13:26:08 -0400

    Solucion de ejercicio propuesto 3

---
+++ tema5-programacion-secuencial/ejercicios-propuestos/ejercicio_propuestos3_tema5.py
@@ -0,0 +1,12 @@
+##Realizar un programa que lea cuatro valores numéricos e informar su suma
+##y promedio.
+
+num1 = float(input("Ingrese el numero 1: "))
+num2 = float(input("Ingrese el numero 2: "))
+num3 = float(input("Ingrese el numero 3: "))
+num4 = float(input("Ingrese el numero 4: "))
+
+suma = num1+num2+num3+num4;
+
+print("La suma de los numeros ingresados es: ",suma)
+print("El promedio de los numeros ingresados es: ",suma/4)
