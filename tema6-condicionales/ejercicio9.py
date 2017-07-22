d786f8d201c809dcf265172438d0845113bb2b56
Isaac Yriarte <isaacyriarte@gmail.com>
2017-07-22 13:39:54 -0400

    Ejercicio 9 de estructura condicionales

---
+++ tema6-condicionales/ejercicio9.py
@@ -0,0 +1,7 @@
+##Ingresar el sueldo de una persona, si supera los 3000 dolares mostrar un
+##mensaje en pantalla indicando que debe abonar impuestos.
+
+sueldo = int(input("Ingrese cual es su sueldo: "))
+
+if sueldo > 3000:
+    print("Esta persona debe pagar impuestos")
