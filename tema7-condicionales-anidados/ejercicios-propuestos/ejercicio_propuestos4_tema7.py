5a19ceb15251a5fa19b2282e56e34ca279e5e281
Isaac Yriarte <isaacyriarte@gmail.com>
2017-07-22 16:37:44 -0400

    Se agrega la descripcion del problema al principio del script

--- tema7-condicionales-anidados/ejercicios-propuestos/ejercicio_propuestos4_tema7.py
+++ tema7-condicionales-anidados/ejercicios-propuestos/ejercicio_propuestos4_tema7.py
@@ -1,3 +1,13 @@
+'''
+Un postulante a un empleo, realiza un test de capacitación, se obtuvo la siguiente información: cantidad total de preguntas que se le realizaron y la 
+cantidad de preguntas que contestó correctamente. Se pide confeccionar un programa que ingrese los dos datos por teclado e informe el nivel del mismo 
+según el porcentaje de respuestas correctas que ha obtenido, y sabiendo que:
+	Nivel máximo:	Porcentaje>=90%.
+	Nivel medio:	Porcentaje>=75% y <90%.
+	Nivel regular:	Porcentaje>=50% y <75%.
+	Fuera de nivel:	Porcentaje<50%.
+'''
+
 cant_preguntas = int(input("Ingrese la cantidad total de preguntas: "))
 
 cant_correctas = int(input("Ingrese la cantidad total de preguntas respondidas correctamente: "))
