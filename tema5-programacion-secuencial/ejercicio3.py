67412a734adf6bea5f51bfe6233f9c069a054071
Isaac Yriarte <isaacyriarte@gmail.com>
2017-07-22 12:02:47 -0400

    Suma y multiplicacion de dos numeros

---
+++ tema5-programacion-secuencial/ejercicio3.py
@@ -0,0 +1,5 @@
+num1 = int(input("Ingrese numero 1: "))
+num2 = int(input("Ingrese numero 2: "))
+
+print ("La suma de los numeros es: ",num1+num2)
+print ("El producto de los numeros es: ",num1*num2)
