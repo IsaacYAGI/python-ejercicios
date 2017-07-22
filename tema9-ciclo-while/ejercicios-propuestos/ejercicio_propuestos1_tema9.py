31a86f81c67c1c5a50af43474efe9a0cecce96cf
Isaac Yriarte <isaacyriarte@gmail.com>
2017-07-22 18:33:51 -0400

    Solucion de ejercicio propuesto 1 tema 9

---
+++ tema9-ciclo-while/ejercicios-propuestos/ejercicio_propuestos1_tema9.py
@@ -0,0 +1,16 @@
+notas_mayores = 0
+notas_menores = 0
+i = 1
+
+while i <=10:
+   # nota_act = float(input("Ingrese nota "+str(i)+": "))
+    nota_act = float(input(f"Ingrese nota {i}: "))
+    if nota_act >= 7:
+        notas_mayores += 1
+    else:
+        notas_menores += 1
+
+    i += 1
+
+print("La cantidad de estudiantes con notas mayores a 7 es de: ",notas_mayores)
+print("La cantidad de estudiantes con notas menores a 7 es de: ",notas_menores)
